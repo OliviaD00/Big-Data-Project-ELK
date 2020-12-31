ead2682a45906063bee4de1f62f9091589145c5a
NeilSCGH <neil.segard@gmail.com>
2020-12-31 15:16:33 +0100

    date added

--- Monitoring_Log_Kafka/log_generator.py
+++ Monitoring_Log_Kafka/log_generator.py
@@ -1,15 +1,22 @@
 import random, time
+from datetime import datetime
 
 types = ["emerg", "error", "notice", "debug"]
 probas= [0.05   , 0.1    , 0.7     , 0.15   ]
 
-while 1:
+def generateType():
     val = random.random()
     for type, proba in zip(types, probas):
-        #print("{} vs {} ({})".format(val, proba, type))
-        if val < proba:
-            print(type)
-            break
-        else:
-            val -= proba
+        if val < proba: return type
+        else: val -= proba
+
+    return "error"
+
+while 1:
+    type = generateType()
+    date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+    msg = "{} [{}]: blabla".format(date,type)
+    print(msg)
+
     time.sleep(0.5)
